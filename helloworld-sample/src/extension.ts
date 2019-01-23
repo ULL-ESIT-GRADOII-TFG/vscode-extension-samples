dfee7f959d55e0e048804790cb6fb52bc12e0559
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2019-01-23 13:35:03 +0000

    Exercise: 1. Give the Hello World command a new name in the Command Palette. 2. Contribute another command that displays current time in an information message. Replace the vscode.window.showInformationMessage with another VS Code API call to show a warning message.

--- helloworld-sample/src/extension.ts
+++ helloworld-sample/src/extension.ts
@@ -1,3 +1,9 @@
+/*
+	Here are some ideas for you to try:
+		Give the Hello World command a new name in the Command Palette.
+		Contribute another command that displays current time in an information message.
+		Replace the vscode.window.showInformationMessage with another VS Code API call to show a warning message.
+*/
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
@@ -12,14 +18,24 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('extension.holaMundo', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		vscode.window.showInformationMessage('Hola VS Code');
+	});
+
+	// The commandId parameter must match the command field in package.json
+	let disposableHora = vscode.commands.registerCommand('extension.hora', () => {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('La fecha actual es: '+Date());
+		vscode.window.showWarningMessage('YOU ARE WARNED!!!');
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposableHora);
 }
 
 // this method is called when your extension is deactivated
